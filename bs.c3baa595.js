c2b4eb8166018f9eda35e0d265957e6f43e914bc
Minsu Kim <minsukim@google.com>
2022-05-06 14:44:46 +0900

    Updates

--- bs.c3baa595.js
+++ bs.c3baa595.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../assets/bg3.jpg":[["bg3.9bd6c1bf.jpg","assets/bg3.jpg"],"assets/bg3.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61978" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57807" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
