1def3c187d2dbf1cada001ad1dadd776223d391f
Minsu Kim <minsukim@google.com>
2022-05-17 12:31:38 +0900

    Updates

--- src.f10117fe.js
+++ src.f10117fe.js
@@ -12936,7 +12936,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62606" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49405" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
